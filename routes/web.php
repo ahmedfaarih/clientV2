27a7a3aee788f452fb607f0fa1de2f6e7ab12984
ahmedfaarih <ahmed.faarih@outlook.com>
2024-04-27 18:22:15 +0500

    website setup done

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,29 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', function() {
+    return view('home');
+})->name('home');
+
+Route::get('/partners', function() {
+    return view('partners');
+})->name('partners');
+
+Route::get('/bots', function() {
+    return view('bots');
+})->name('bots');
+
+Route::get('/areas', function() {
+    return view('areas');
+})->name('areas');
+
+Route::get('/values', function() {
+    return view('values');
+})->name('values');
+
+Route::get('/publications', function() {
+    return view('publications');
+})->name('publications');
+
+
