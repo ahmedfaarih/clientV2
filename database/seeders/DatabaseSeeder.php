5300e108fca5e1105e2c2c23b2c442d1cde6ef7d
Raahif Rafeeu <raahif.rafeeu@gmail.com>
2024-05-05 11:33:01 +0500

    Fixex

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
         $adminRole = Role::firstOrCreate(['name' => 'Super Admin']);
+        $userRole = Role::firstOrCreate(['name' => 'user']);
 
         $user = User::factory()->create([
             'name' => 'Test User',
